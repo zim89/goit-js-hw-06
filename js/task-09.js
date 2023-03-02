604757283391fde2391857f7215c9efb66cc4d67
zim89 <121241841+zim89@users.noreply.github.com>
2023-03-02 19:16:06 +0200

    10 finish

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  btnEl: document.querySelector('.change-color'),
+  colorEl: document.querySelector('.color'),
+};
+
+refs.btnEl.addEventListener('click', onChangeColorClick);
+
+function onChangeColorClick() {
+  const getColor = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = getColor;
+  refs.colorEl.textContent = getColor;
+}
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  input: document.querySelector('input'),
+  btnCreate: document.querySelector('[data-create]'),
+  btnDestroy: document.querySelector('[data-destroy]'),
+  boxesElement: document.querySelector('#boxes'),
+};
+
+refs.btnCreate.addEventListener('click', onCreateClick);
+refs.btnDestroy.addEventListener('click', onDestroyClick);
+
+function onCreateClick() {
+  let boxesList = '';
+  let size = 30;
+
+  for (let i = 1; i <= refs.input.value; i += 1) {
+    boxesList += `<div style = 'width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}; margin: 10px'></div>`;
+    size += 10;
+  }
+
+  refs.boxesElement.insertAdjacentHTML('afterbegin', boxesList);
+}
+
+function onDestroyClick() {
+  refs.boxesElement.innerHTML = '';
+}
